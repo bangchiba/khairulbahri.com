aedf1e75f714bf87de0f2c95ccc7e8712a6a2a04
Khairul Bahri <khairul@youexpress.co.id>
2019-12-12 16:48:59 +0700

    initial commit

---
+++ config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+    pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+  
+    siteTitle: "Khairul Bahri", // Navigation and Site Title
+    siteTitleAlt: "Khairul Bahri - Full stack web developer", // Alternative Site title for SEO
+    siteTitleShort: "Khairul Bahri", // short_name for manifest
+    siteHeadline: "Full stack web developer from Germany", // Headline for schema.org JSONLD
+    siteUrl: "https://khairulbahri.com", // Domain of your site. No trailing slash!
+    siteLanguage: "en", // Language Tag on <html> element
+    siteLogo: "/logo.png", // Used for SEO and manifest
+    siteDescription: "Khairul Bahri, Freelance web developer from Germany",
+    author: "Khairul Bahri <khairulbahri.aceh@gmail.com>", // Author for schema.org JSONLD
+  
+    keywords:
+      "Khairul Bahri, web developer, javascript, node.js, reactjs",
+  
+    // siteFBAppID: '123456789', // Facebook App ID - Optional
+    userTwitter: "@bangchiba_", // Twitter Username
+    ogSiteName: "khairulbahri", // Facebook Site Name
+    ogLanguage: "en_EN", // og:language
+    // googleAnalyticsID: "UA-111816913-1"
+  }
+  
---
+++ src/pages/blog.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Link, graphql } from "gatsby";
+import styled from "styled-components";
+import groupBy from "lodash/groupBy";
+import Section from "../components/Shared/Section/Section";
+import Layout from "../components/Layout";
+import SEO from "../components/seo";
+
+const BlogOverview = styled.div`
+  a {
+    text-decoration: none;
+  }
+
+  a:hover {
+    border-bottom: 1px solid;
+  }
+
+  h3 {
+    margin-bottom: 0.725em;
+  }
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+  }
+`;
+
+const BlogPage = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+  const groupedPosts = groupBy(posts, post => post.node.frontmatter.year);
+
+  return (
+    <Layout>
+      <SEO title={`Blog`} />
+      <Section
+        style={{ position: "relative", paddingTop: "40px", padding: "10px" }}
+      >
+        <h1>Writings</h1>
+        {Object.keys(groupedPosts)
+
+          .reverse()
+          .map(year => (
+            <BlogOverview key={year}>
+              <h3>{year}</h3>
+              <ul>
+                {groupedPosts[year]
+                  .sort((a, b) => {
+                    console.log(a, b);
+                    return a.node.frontmatter.order > b.node.frontmatter.order
+                      ? 1
+                      : -1;
+                  })
+                  .map(p => (
+                    <li key={p.node.id}>
+                      <Link to={p.node.frontmatter.path}>
+                        {p.node.frontmatter.title}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </BlogOverview>
+          ))}
+      </Section>
+    </Layout>
+  );
+};
+
+export default BlogPage;
+
+// export const postsQuery = graphql`
+//   query AllBlogPosts {
+//     allMarkdownRemark(
+//       limit: 10
+//       sort: { fields: [frontmatter___date], order: ASC }
+//       filter: { frontmatter: { type: { eq: "post" }, published: { eq: true } } }
+//     ) {
+//       edges {
+//         node {
+//           id
+//           frontmatter {
+//             order
+//             path
+//             title
+//             published
+//             date
+//             year
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
